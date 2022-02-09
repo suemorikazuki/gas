78040d4c1f005d0cc04385a3b4c6be4989c67f5a
suemorikazuki <kazuki.suemori.work@gmail.com>
2022-02-09 12:06:22 +0900

    slack通知

--- gas6.js
+++ gas6.js
@@ -1,160 +1,235 @@
-const UPDATE_COL = 3;
-const STRING_COL = 4;
-const MEMBER_COL = 6;
-const COMPLETE_DATE_COL = 7;
-const LIMIT_DATE_COL =8;
-const STATUS_COL = 9;
-const CHANGE_DATE_COL = 12;
-const DELETE_COL = 13;
+const IS_UPDATE_COL = 2;
+const DATA_START = 3;
+const MEMBER_COL = 5;
+const COMPLETE_DATE_COL = 6;
+const DUE_COL =7;
+const STATUS_COL = 8;
+const CREATER_COL = 9;
+const CREATE_DATE_COL = 10;
+const CHANGE_DATE_COL = 11;
+const IS_DELETE_COL = 12;
 const STATUS = ['未対応', '対応中', '対応済み', '完了'];
-// STATUS辞書にしない？
+const TASK_SHEET = 'プロジェクト_中村';
+const USER_SHEET = 'メンバー';
 
-function getSheet(sheetName) {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().  getSheetByName(sheetName);
-  return sheet;
+function test() {
+  date = Utilities.formatDate(new Date(), "Asia/Tokyo", "MM/dd");
+  console.log(new Date());
 }
 
-function getTaskCell() {
-  const sheet = getSheet('プロジェクト_中村');
-  const lastRow = sheet.getLastRow();
-
-  for (let i = 7;i <= lastRow; i++) {
-    const value = sheet.getRange(i, stringCol).getValue();
-    if (value != '') {
-      insertMemberList(i, MEMBER_COL, sheet);
-      insertStatusList(i, STATUS_COL, sheet);
-    }
+function isString(value) {
+  if (typeof value === "string" || value instanceof String) {
+    return true;
+  } else {
+    return false;
   }
 }
 
+function getDateByString(_date){
+  if(isString(_date)){
+    _date = _date.replace('⚫︎ ', '');
+  } else {
+    _date = Utilities.formatDate(new Date(_date), "Asia/Tokyo", "MM/dd");
+  }
+  return _date;
+}
+
+function getDiffDate(i){
+  let sheet = getSheet(TASK_SHEET);
+  let today = new Date(Utilities.formatDate(new Date(), "Asia/Tokyo", "MM/dd"));
+  let date2 = new Date(getDateByString(sheet.getRange(i, DUE_COL).getValue()));
+
+  let diffDate = (date2 - today) / (60 * 60 * 24 * 1000);
+  return diffDate;
+}
+// 以下修正前
+function setMark(){
+  let sheet = getSheet(TASK_SHEET);
+  lastRow = sheet.getLastRow();
+  for(let i= DATA_START; i <= lastRow; i++){
+    let diffDate = getDiffDate(i);
+    let status = sheet.getRange(i, STATUS_COL).getDisplayValue();
+    let compStatus = STATUS[3];
+    if(diffDate < 1 && status != compStatus){
+      sheet.getRange(i, DATA_START).setBackground("#FF3333");
+      let notification = 'attention'
+    }else if(diffDate < 3 && status != compStatus){
+      sheet.getRange(i, DATA_START).setBackground("#FFFF66");
+      let notification = 'warning'
+    }
+  }
+  return notification
+}
+
+// 以下修正後？
+function setMark(){
+  let sheet = getSheet(TASK_SHEET);
+  lastRow = sheet.getLastRow();
+  let warning = []
+  let attention = []
+  for(let i= DATA_START; i <= lastRow; i++){
+    let diffDate = getDiffDate(i);
+    let status = sheet.getRange(i, STATUS_COL).getDisplayValue();
+    let compStatus = STATUS[3];
+    let taskName = sheet.getRange(i, DATA_START).getDisplayValue();
+    
+    if(diffDate < 1 && status != compStatus){
+      sheet.getRange(i, DATA_START).setBackground("#FF3333");
+      warning.push(taskName)
+    }else if(diffDate < 3 && status != compStatus){
+      sheet.getRange(i, DATA_START).setBackground("#FFFF66");
+      attention.push(taskName)
+    }else if(status == compStatus) {
+      sheet.getRange(i, DATA_START).setBackground("#D9D9D9");
+    }
+  }
+  return [warning, attention]
+}
+
+function crateMessage() {
+  notification = setMark()
+  message = 
+  '締め切りが過ぎているタスク' +
+  ' : ' +
+  notification[0] + 
+  ' / ' +
+  '締め切り間近なタスク' +
+  ' : ' +
+  notification[1] 
+  return message
+}
+//修正案ここまで
+
+function getSheet(sheetName) {
+  let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+  return sheet;
+}
+
 function createMemberList() {
-  const memberSheet = getSheet('メンバー');
-  const lastRow = memberSheet.getLastRow();
-  const memberSheetCol = 2;
-  const memberSheetRow = 2;
-  const memberRange = memberSheet.getRange(memberSheetRow, memberSheetCol, lastRow);
-  const memberList = SpreadsheetApp.newDataValidation().requireValueInRange(memberRange).build();
+  let memberSheet = getSheet(USER_SHEET);
+  let lastRow = memberSheet.getLastRow();
+  let startCol = 1;
+  let startRow = 2;
+  let memberRange = memberSheet.getRange(startRow, startCol, lastRow);
+  let memberList = SpreadsheetApp.newDataValidation().requireValueInRange(memberRange).build();
   return memberList;
 }
 
 function insertMemberList(row, col, sheet) {
-  const memberList = createMemberList(); 
-  const cell = sheet.getRange(row,col);
-  cell.setDataValidation(memberList);
+  let memberList = createMemberList(); 
+  let setCell = sheet.getRange(row,col);
+  setCell.setDataValidation(memberList);
 }
-　
+
 function insertStatusList(row, col, sheet) {
-  const statusList = SpreadsheetApp.newDataValidation().requireValueInList(STATUS).build();
-  const cell = sheet.getRange(row, col);
-  cell.setDataValidation(statusList);
+  let statusList = SpreadsheetApp.newDataValidation().requireValueInList(STATUS).build();
+  let setCell = sheet.getRange(row, col);
+  setCell.setDataValidation(statusList);
+}
+
+function insertLists(row, sheet) {
+  insertMemberList(row, MEMBER_COL, sheet);
+  insertStatusList(row, STATUS_COL, sheet);
+}
+
+
+function setDate(row, col, sheet) {
+  const now = new Date();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  sheet.getRange(row, col).setValue(month + '/' + date);
+}
+
+function getDeleteRowList(lastRow, sheet) {
+  let deleteRowList = [];
+  for (let i = 1; i <= lastRow; i++) {
+    let isDeleteValue = sheet.getRange(i, IS_DELETE_COL).getValue();
+    if (isDeleteValue == true) {
+      deleteRowList.push(i);
+    } 
+  }
+  return deleteRowList;
+}
+
+function deleteTask(lastRow, sheet) {
+  let deleteRowList = getDeleteRowList(lastRow, sheet);
+  deleteRowList.forEach(row => {
+    sheet.deleteRow(row);
+  })
+}
+
+function setStatusColor(row, sheet) {
+  const colors = ["#FFFFFF", "#d9ead3", "#cfe2f3", "#d9d9d9"];
+  let status = sheet.getRange(row, STATUS_COL).getValue();
+  let lastCol = sheet.getLastColumn();
+  let setColorRange = sheet.getRange(row, DATA_START, 1, lastCol);
+    
+  if (status == STATUS[0]) {
+    setColorRange.setBackground(colors[0]);
+  } else if (status == STATUS[1]) {
+    setColorRange.setBackground(colors[1]);
+  } else if (status == STATUS[2]) {
+    setColorRange.setBackground(colors[2]); 
+  } else if (status == STATUS[3]) {
+    setColorRange.setBackground(colors[3]); 
+  }
+}
+
+function updateTask(row, sheet){
+  for (let i = 1; i <= row; i++) {
+    let updateRange = sheet.getRange(i, IS_UPDATE_COL);
+    let isUpdateValue = updateRange.getValue();
+    if (isUpdateValue == true) {
+      updateRange.uncheck();
+      setStatusColor(i, sheet);
+      setDate(i, CHANGE_DATE_COL, sheet);
+    }
+  }
+}
+
+function setCreater(createRow, sheet) {
+  let message = '作成者の名前を入力して下さい。';
+  let creater = Browser.inputBox(message);
+  if(creater == cancel){
+    return;
+  }else{
+    sheet.getRange(createRow, CREATER_COL).setValue(creater);
+  }
+}
+
+function addTask() {
+  let sheet = getSheet(TASK_SHEET);
+  let createRow = sheet.getLastRow() + 1;
+  sheet.getRange(createRow, IS_UPDATE_COL).insertCheckboxes();
+  sheet.getRange(createRow, IS_DELETE_COL).insertCheckboxes();
+  sheet.getRange(createRow, COMPLETE_DATE_COL, 1, 2).setNumberFormat("MM/dd");
+  insertLists(createRow, sheet);
+  setDate(createRow, CREATE_DATE_COL, sheet);
+  setCreater(createRow, sheet)
+}
+
+function updateInfo() {
+  let sheet = getSheet(TASK_SHEET);
+  let lastRow = sheet.getLastRow();
+  deleteTask(lastRow, sheet);
+  updateTask(lastRow, sheet);
 }
 
 function onOpen() {
   const ui = SpreadsheetApp.getUi()
   const menu = ui.createMenu("メニュー");
-  menu.addItem("実行","reflectChange");
-  menu.addItem("新規タスク","addTask");
+  menu.addItem("更新","updateInfo");
+  menu.addItem("新規タスク作成","addTask");
   menu.addToUi();
 }
 
-function setTask() {
-  const sheet = getSheet('プロジェクト_中村')
-  let lastRow = sheet.getLastRow() - 1
-  let copyRow = sheet.getLastRow()
-  sheet.getRange(lastRow, 3, lastRow, 14).copyTo(sheet.getRange(copyRow, 3))
-  sheet.getRange(copyRow, 4, copyRow, 13).clearContent()
-}
-
-function reflectChange() {
-  const sheet = getSheet('プロジェクト_中村');
-  const lastRow = sheet.getLastRow();
-  const deleteCells = [];
-
-  for (let i = 1;i <= lastRow; i++) {
-    const updateRange = sheet.getRange(i, UPDATE_COL);
-    const deleteRange = sheet.getRange(i, DALETECOL);
-    
-    if (deleteRange.getValue() == true) {
-      deleteCells.unshift(i);
-    } else if (updateRange.getValue() == true) {
-      updateRange.uncheck();
-      changeStatusColor(i, sheet);
-      changeDate(i, sheet);
-    }
-  }
-  deleteTask(deleteCells, sheet)
-}
-
-function changeStatusColor(row, sheet) {
-  const value = sheet.getRange(row, STATUS_COL).getValue();
-  const lastCol = sheet.getLastColumn();
-
-  if (value == STATUS[0]) {
-    sheet.getRange(row, STRINGCOL, 1, lastCol).setBackground("#FFFFFF");
-  } else if (value == STATUS[1]) {
-    sheet.getRange(row, STRINGCOL, 1, lastCol).setBackground("#d9ead3");
-  } else if (value == STATUS[2]) {
-    sheet.getRange(row, STRINGCOL, 1, lastCol).setBackground("#cfe2f3"); 
-  } else if (value == STATUS[3]) {
-    sheet.getRange(row, STRINGCOL, 1, lastCol).setBackground("#d9d9d9"); 
-  }
-}
-
-function deleteTask( deleteCells, sheet ) {
-  deleteCells.forEach(row => {
-      sheet.deleteRows(row);
-  })
-}
-
-function changeDate(changeDateRow, sheet) {
-  const now = new Date();
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
-  sheet.getRange( changeDateRow, CHANGE_DATE_COL ).setValue( month + '/' + date_ );
-}
-
-function isntBlank(val) {
-  if (val[0] != '') return val
-}
-
-function showLimit() {
-  const sheet = getSheet('プロジェクト_中村');
-  // const sheet = SpreadsheetApp.getActiveSheet()
-  const val = sheet.getRange( 1, COMPLETE_DATE_COL, sheet.getLastRow() ).ge_tVal_ues();
-  const values = val.filter(isntBlank);
-  for (let i = 0; i < values.length; i++) {
-    // const target = new Date(values.getFullYear(), Val.getMonth(), Val.getDate())
-    const now = new Date()
-    // const today = new Date(now.getFullYear(),now.getMonth(),now.getDate())
-    // console.log((target - today)/60*60*24)
-    // return (target - today)/60*60*24
-  }
-}
-
-function changeColor( row, col , color ) {
-  let sheet = getSheet('プロジェクト_中村')
-  sheet.getRange(row, col).setFontColor( color )
-}
-
-function getCellValue( row, col) {
-  let sheet = getSheet('プロジェクト_中村')
-  let cellValue = sheet.getRange(row, col).getValue()
-  return cellValue
-}
-
-function addString() {
-  let pointedValue =  '●' + getCellValue(7,7) 
-  return pointedValue
-}
 
 
-// slack連携
-const webhookUrl = 'https://hooks.slack.com/services/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';
-const username = 'username';  // 通知時に表示されるユーザー名
-const icon = ':hatching_chick:';  // 通知時に表示されるアイコン
-let message = 'test';  // 投稿メッセージ
-
-function myFunction() {
+function slack() {
+  const webhookUrl = 'https://hooks.slack.com/services/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';
+  const username = 'username';  // 通知時に表示されるユーザー名
+  const icon = ':hatching_chick:';  // 通知時に表示されるアイコン
+  let message = crateMessage();  // 投稿メッセージ
   let jsonData =
   {
      "username" : username,
